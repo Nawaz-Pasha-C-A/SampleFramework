523e17c433266d1f06039e135f184a97f2e90da5
Nawaz Pasha <Nawaz Pasha@Nawaz-PC>
2021-05-08 21:25:24 +0530

    Project has been commited

---
+++ Sample_Project/src/test/java/com/crm/comcast/productTest/CreateProductTest.java
@@ -0,0 +1,11 @@
+package com.crm.comcast.productTest;
+
+import org.testng.annotations.Test;
+
+public class CreateProductTest {
+
+	@Test
+	public void createProduct(){
+		System.out.println("Product has been created");
+	}
+}
---
+++ Sample_Project/src/test/java/com/crm/comcast/productTest/EditProductTest.java
@@ -0,0 +1,12 @@
+package com.crm.comcast.productTest;
+
+import org.testng.annotations.Test;
+
+public class EditProductTest {
+	@Test
+	public void editProduct()
+	{
+		System.out.println("product edited successfully");
+	}
+
+}
