ec47ad2bfbcd66b32af660d9bf55f708de81d931
Nawaz Pasha <Nawaz Pasha@Nawaz-PC>
2021-05-09 15:41:52 +0530

    Maven profiling

--- SampleProject2/src/test/java/com/crm/comcast/product/Test/CreateProductTest.java
+++ SampleProject2/src/test/java/com/crm/comcast/product/Test/CreateProductTest.java
@@ -3,10 +3,15 @@ package com.crm.comcast.product.Test;
 import org.testng.annotations.Test;
 
 public class CreateProductTest {
-	@Test
+	@Test(groups="smokeTest")
 	public void createProduct()
 	{
 		System.out.println("Product has been created");
 	}
 
+	@Test(groups="regressionTest")
+	public void productQty()
+	{
+		System.out.println("80 MILLION TONS");
+	}
 }
--- SampleProject2/src/test/java/com/crm/comcast/product/Test/EditProductTest.java
+++ SampleProject2/src/test/java/com/crm/comcast/product/Test/EditProductTest.java
@@ -3,10 +3,14 @@ package com.crm.comcast.product.Test;
 import org.testng.annotations.Test;
 
 public class EditProductTest {
-	@Test
+	@Test(groups="smokeTest")
 	public void editProduct()
 	{
 		System.out.println("Product has been edited Succesffuly");
 	}
-
+    @Test(groups="regressionTest")
+    public void productDeployedTo()
+    {
+    	System.out.println("Syndney,Austrailia");
+    }
 }
